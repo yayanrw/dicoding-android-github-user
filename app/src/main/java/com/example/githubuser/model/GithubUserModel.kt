a8b7750163244131d1919679033da92e0c0c2da6
yayan-msglow <yayanrahmat@msglowid.com>
2021-10-27 14:00:43 +0700

    mod model to parcelize

--- app/src/main/java/com/example/githubuser/model/GithubUserModel.kt
+++ app/src/main/java/com/example/githubuser/model/GithubUserModel.kt
@@ -1,5 +1,9 @@
 package com.example.githubuser.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GithubUserModel (
     val username: String,
     val name: String,
@@ -9,4 +13,4 @@ data class GithubUserModel (
     val repository: Long,
     val follower: Long,
     val following: Long
-)
+) : Parcelable
